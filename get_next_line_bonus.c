69cc6c9b7ab212620c9c0cedf1c41799b544a670
Manu Ardeiro <102896906+ManuArdeiro@users.noreply.github.com>
2022-05-05 17:17:40 +0200

    Update get_next_line_bonus.c

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -9,19 +9,19 @@ char	*ft_read_to_left_str(int fd, char *str)
 	if (!buffer)
 		return (NULL);
 	read_bytes = 1;
-	while (!ft_strchr(str, '\n') && rd_bytes != 0)
+	while (!ft_strchr(str, '\n') && read_bytes != 0)
 	{
-		rd_bytes = read(fd, buff, BUFFER_SIZE);
-		if (rd_bytes == -1)
+		read_bytes = read(fd, buffer, BUFFER_SIZE);
+		if (read_bytes == -1)
 		{
-			free(buff);
+			free(buffer);
 			return (NULL);
 		}
-		buff[rd_bytes] = '\0';
-		left_str = ft_strjoin(left_str, buff);
+		buffer[read_bytes] = '\0';
+		str = ft_strjoin(str, buffer);
 	}
-	free(buff);
-	return (left_str);
+	free(buffer);
+	return (str);
 }
 
 char	*get_next_line(int fd)
@@ -34,7 +34,7 @@ char	*get_next_line(int fd)
 	str[fd] = ft_read_str(fd, str[fd]);
 	if (!str[fd])
 		return (NULL);
-	line = ft_get_line(left_str[fd]);
-	left_str[fd] = ft_new_left_str(left_str[fd]);
+	line = ft_get_line(str[fd]);
+	str[fd] = ft_new_str(str[fd]);
 	return (line);
 }
