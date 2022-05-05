c4ff0d79ad491bc9279ac2b3e13a772c08431047
Manu Ardeiro <102896906+ManuArdeiro@users.noreply.github.com>
2022-05-05 17:14:41 +0200

    Create get_next_line.c

---
+++ get_next_line.c
@@ -0,0 +1,40 @@
+#include "get_next_line.h"
+
+char	*ft_read_to_left_str(int fd, char *str)
+{
+	char	*buffer;
+	int		read_bytes;
+
+	buffer = malloc(sizeof(char) * BUFFER_SIZE + 1);
+	if (!buffer)
+		return (NULL);
+	read_bytes = 1;
+	while (!ft_strchr(str, '\n') && read_bytes != 0)
+	{
+		read_bytes = read(fd, buffer, BUFFER_SIZE);
+		if (read_bytes == -1)
+		{
+			free(buffer);
+			return (NULL);
+		}
+		buffer[read_bytes] = '\0';
+		str = ft_strjoin(str, buffer);
+	}
+	free(buffer);
+	return (str);
+}
+
+char	*get_next_line(int fd)
+{
+	char      *line;
+	static char *str;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (0);
+	str = ft_read_str(fd, str);
+	if (!str)
+		return (NULL);
+	line = ft_get_line(str);
+	str = ft_new_str(str);
+	return (line);
+}
