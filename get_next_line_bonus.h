1e93d9d56dcd1ea950773e7a573c6d53d914962a
Manu Ardeiro <102896906+ManuArdeiro@users.noreply.github.com>
2022-05-08 13:14:45 +0200

    Update get_next_line_bonus.h

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -1,5 +1,5 @@
-#ifndef GET_NEXT_LINE_BONUS_H
-# define GET_NEXT_LINE_BONUS_H
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
 # include <stdlib.h>
 # include <stdio.h>
@@ -10,11 +10,11 @@
 # endif
 
 char	*get_next_line(int fd);
-char	*ft_read_str(int fd, char *str);
+char	*ft_read_buffer(int fd, char *str);
+char	*ft_get_line(char *container);
+char	*ft_new_container(char *container);
+size_t	ft_str_len(char *s);
 char	*ft_str_chr(char *s, int c);
 char	*ft_str_join(char *str, char *buffer);
-size_t	ft_strlen(char *s);
-char	*ft_get_line(char *str);
-char	*ft_new_str(char *str);
 
 #endif
