53264c52bb40fe0d6f8d4b8e472ce550b52350c1
Manu Ardeiro <102896906+ManuArdeiro@users.noreply.github.com>
2022-05-08 13:16:35 +0200

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -1,40 +1,95 @@
 #include "get_next_line.h"
 
-char	*ft_read_str(int fd, char *str)
+char	*ft_new_container(char *container)
+{
+	int		i;
+	int		j;
+	char	*sol;
+
+	i = 0;
+	while (container[i] != '\0' && container[i] != '\n')
+		i++;
+	if (!container[i])
+	{
+		free(container);
+		return (NULL);
+	}
+	sol = malloc(sizeof(char) * (ft_str_len(container) - i + 1));
+	if (!sol)
+		return (NULL);
+	i++;
+	j = 0;
+	while (container[i])
+	{
+		sol[j] = container[i];
+		i++;
+		j++;
+	}
+	sol[j] = '\0';
+	free(container);
+	return (sol);
+}
+
+char	*ft_get_line(char *container)
+{
+	int		i;
+	char	*sol;
+
+	i = 0;
+	if (!container)
+		return (NULL);
+	while (container[i] != '\0' && container[i] != '\n')
+		i++;
+	sol = malloc(sizeof(char) * i + 2);
+	if (!sol)
+		return (NULL);
+	i = 0;
+	while (container[i] != '\0' && container[i] != '\n')
+	{
+		sol[i] = container[i];
+		i++;
+	}
+	sol[i] = container[i];
+	i++;
+	sol[i] = '\0';
+	return (sol);
+}
+
+char	*ft_read_buffer(int fd, char *container)
 {
 	char	*buffer;
-	int		read_bytes;
+	int		buffer_len;
 
 	buffer = malloc(sizeof(char) * BUFFER_SIZE + 1);
 	if (!buffer)
 		return (NULL);
-	read_bytes = 1;
-	while (!ft_str_chr(str, '\n') && read_bytes != 0)
+	buffer_len = 1;
+	while (!ft_str_find_chr(container, '\n') && buffer_len != 0)
 	{
-		read_bytes = read(fd, buffer, BUFFER_SIZE);
-		if (read_bytes == -1)
+		buffer_len = read(fd, buffer, BUFFER_SIZE);
+		if (buffer_len == -1)
 		{
 			free(buffer);
 			return (NULL);
 		}
-		buffer[read_bytes] = '\0';
-		str = ft_str_join(str, buffer);
+		buffer[buffer_len] = '\0';
+		container = ft_str_join_buffer(container, buffer);
 	}
 	free(buffer);
-	return (str);
+	return (container);
 }
 
 char	*get_next_line(int fd)
 {
-	char      *line;
-	static char *str;
+	char		*line;
+	static char	*container;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (0);
-	str = ft_read_str(fd, str);
-	if (!str)
+	container = ft_read_buffer(fd, container);
+	if (!container[fd])
 		return (NULL);
-	line = ft_get_line(str);
-	str = ft_new_str(str);
+	line = ft_get_line(container[fd]);
+	container = ft_new_container(container);
 	return (line);
 }
